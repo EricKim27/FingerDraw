fc02694eb44c281f8b62caf06f90e045dc7e9ac0
EricKim27 <seohyun.kim@outlook.kr>
2024-07-30 15:31:56 +0900

    verbose logging

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ try:
     print("")
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands()
+    print("Starting Camera...")
     cap = cv2.VideoCapture(1)
     cv2.startWindowThread()
     text = "Black"
